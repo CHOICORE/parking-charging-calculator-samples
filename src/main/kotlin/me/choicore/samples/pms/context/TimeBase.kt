20dc8dacf4e0cbee9a70d2e6b5c75857158aa657
CHOICORE <choijaehyeong93@gmail.com>
2024-10-02 17:46:14 +0900

    test: Timebase 클래스 네임 TimeBase 로 변경 및 관련 로직 검증

--- src/main/kotlin/me/choicore/samples/pms/context/TimeBase.kt
+++ src/main/kotlin/me/choicore/samples/pms/context/TimeBase.kt
@@ -3,7 +3,7 @@ package me.choicore.samples.pms.context
 import java.time.Duration
 import java.time.LocalTime
 
-data class Timebase(
+data class TimeBase(
     val from: LocalTime,
     val to: LocalTime,
 ) {
@@ -13,26 +13,24 @@ data class Timebase(
     val isFullTime: Boolean
         get() = from == LocalTime.MIN && to == LocalTime.MAX || from == to
 
-    fun duration(): Long {
-        return if (isFullTime) {
+    fun duration(): Long =
+        if (isFullTime) {
             FULL_DAY_OF_MINUTES
         } else if (crossesMidnight) {
             (Duration.between(from, LocalTime.MAX) + Duration.between(LocalTime.MIN, to)).toMinutes() + 1
         } else {
             Duration.between(from, to).toMinutes()
         }
-    }
 
-    operator fun contains(time: LocalTime): Boolean {
-        return if (crossesMidnight) {
+    operator fun contains(time: LocalTime): Boolean =
+        if (crossesMidnight) {
             time >= from || time <= to
         } else {
             time in from..to
         }
-    }
 
     companion object {
         const val FULL_DAY_OF_MINUTES = 24 * 60L
-        val FULL_TIME = Timebase(LocalTime.MIN, LocalTime.MAX)
+        val FULL_TIME = TimeBase(LocalTime.MIN, LocalTime.MAX)
     }
 }
--- src/test/kotlin/me/choicore/samples/pms/charger/Tests.kt
+++ src/test/kotlin/me/choicore/samples/pms/charger/Tests.kt
@@ -1,7 +1,9 @@
 package me.choicore.samples.pms.charger
 
-import me.choicore.samples.pms.context.Timebase
+import me.choicore.samples.pms.context.TimeBase
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.DayOfWeek
 import java.time.Duration
@@ -10,171 +12,325 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 
 class Tests {
-    @Test
-    @DisplayName("시간 범위가 하루를 넘어가는 경우")
-    fun t2() {
-        val arrivedAt = LocalDateTime.of(2024, 10, 1, 16, 0)
-        val departedAt = LocalDateTime.of(2024, 10, 3, 12, 0)
-        val results = apply(arrivedAt, departedAt, Timebase.FULL_TIME)
-        results.forEach {
-            println("$it, 적용 시간: ${it.toMinutes} 분")
+    @Nested
+    inner class OneDayTests {
+        @Test
+        @DisplayName("arrivedAt > timeBase.from && timeBase.to < departedAt")
+        fun t1() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 30)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 18, 1)
+
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
+
+            val results = apply(arrivedAt, departedAt, timeBase)
+
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 9, 30))
+                assertThat(it.to)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 18, 0))
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
         }
 
-        val totalSumOfDurations = results.sumOf { it.toMinutes }
-        println("총 적용 시간: $totalSumOfDurations")
-    }
+        @Test
+        @DisplayName("arrivedAt > timeBase.from && timeBase.to > departedAt")
+        fun t2() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 30)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 17, 59)
 
-    @Test
-    fun t3() {
-        val arrivedAt = LocalDateTime.of(2024, 10, 1, 16, 30)
-        val departedAt = LocalDateTime.of(2024, 10, 1, 21, 0)
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
 
-        val timebase = Timebase(LocalTime.of(9, 0), LocalTime.of(18, 0))
+            val results = apply(arrivedAt, departedAt, timeBase)
 
-        val results = apply(arrivedAt, departedAt, timebase)
-        results.forEach {
-            println("$it, 적용 시간: ${it.toMinutes}분")
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 9, 30))
+                assertThat(it.to)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 17, 59))
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
         }
 
-        val totalSumOfDurations = results.sumOf { it.toMinutes }
-        println("총 적용 시간: ${totalSumOfDurations}분")
-    }
+        @Test
+        @DisplayName("arrivedAt > timeBase.from && timeBase.to == departedAt")
+        fun t3() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 30)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 18, 0)
 
-    @Test
-    fun t4() {
-        val arrivedAt = LocalDateTime.of(2024, 10, 1, 18, 0)
-        val departedAt = LocalDateTime.of(2024, 10, 2, 0, 30)
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
 
-        val timebase = Timebase(LocalTime.of(23, 0), LocalTime.of(1, 0))
+            val results = apply(arrivedAt, departedAt, timeBase)
 
-        val results = apply(arrivedAt, departedAt, timebase)
-        results.forEach {
-            println("$it, 적용 시간: ${it.toMinutes}분")
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 9, 30))
+                assertThat(it.to)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 18, 0))
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
         }
 
-        val totalSumOfDurations = results.sumOf { it.toMinutes }
-        println("총 적용 시간: ${totalSumOfDurations}분")
-    }
+        @Test
+        @DisplayName("arrivedAt == timeBase.from && timeBase.to < departedAt")
+        fun t4() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 0)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 19, 0)
 
-    @Test
-    fun t5() {
-        val arrivedAt = LocalDateTime.of(2024, 10, 1, 23, 0)
-        val departedAt = LocalDateTime.of(2024, 10, 11, 2, 0)
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
 
-        val timebase = Timebase(LocalTime.of(23, 0), LocalTime.of(1, 30))
+            val results = apply(arrivedAt, departedAt, timeBase)
 
-        val results = apply(arrivedAt, departedAt, timebase)
-        results.forEach {
-            println("$it, 적용 시간: ${it.toMinutes}분")
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(arrivedAt)
+                assertThat(it.to)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 18, 0))
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
         }
 
-        val totalSumOfDurations = results.sumOf { it.toMinutes }
-        println("총 적용 시간: ${totalSumOfDurations}분")
-    }
+        @Test
+        @DisplayName("arrivedAt == timeBase.from && timeBase.to > departedAt")
+        fun t5() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 0)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 17, 59)
 
-    @Test
-    fun t6() {
-        val arrivedAt = LocalDateTime.of(2024, 10, 1, 23, 30)
-        val departedAt = LocalDateTime.of(2024, 10, 3, 2, 30)
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
 
-        val timebase = Timebase(LocalTime.of(23, 0), LocalTime.of(1, 30))
+            val results = apply(arrivedAt, departedAt, timeBase)
 
-        val results = apply(arrivedAt, departedAt, timebase)
-        results.forEach {
-            println("$it, 적용 시간: ${it.toMinutes}분")
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(arrivedAt)
+                assertThat(it.to)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 17, 59))
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
         }
 
-        val totalSumOfDurations = results.sumOf { it.toMinutes }
-        println("총 적용 시간: ${totalSumOfDurations}분")
-    }
+        @Test
+        @DisplayName("arrivedAt == timeBase.from && timeBase.to == departedAt")
+        fun t6() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 0)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 18, 0)
 
-    @Test
-    fun t7() {
-        val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 0)
-        val departedAt = LocalDateTime.of(2024, 10, 3, 21, 0)
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
 
-        val timebase = Timebase(LocalTime.of(18, 0), LocalTime.of(22, 30))
+            val results = apply(arrivedAt, departedAt, timeBase)
 
-        val results = apply(arrivedAt, departedAt, timebase)
-        results.forEach {
-            println("$it, 적용 시간: ${it.toMinutes}분")
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(arrivedAt)
+                assertThat(it.to)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 18, 0))
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
         }
 
-        val totalSumOfDurations = results.sumOf { it.toMinutes }
-        println("총 적용 시간: ${totalSumOfDurations}분")
-    }
+        @Test
+        @DisplayName("arrivedAt < timeBase.from && timeBase.to == departedAt")
+        fun t7() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 1)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 18, 0)
 
-    @Test
-    fun t8() {
-        val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 0)
-        val departedAt = LocalDateTime.of(2024, 10, 3, 21, 0)
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
 
-        val timebase = Timebase(LocalTime.of(18, 0), LocalTime.of(22, 30))
+            val results = apply(arrivedAt, departedAt, timeBase)
 
-        val results = apply(arrivedAt, departedAt, timebase)
-        results.forEach {
-            println("$it, 적용 시간: ${it.toMinutes}분")
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 9, 1))
+                assertThat(it.to)
+                    .isEqualTo(departedAt)
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
         }
 
-        val totalSumOfDurations = results.sumOf { it.toMinutes }
-        println("총 적용 시간: ${totalSumOfDurations}분")
+        @Test
+        @DisplayName("arrivedAt < timeBase.from && timeBase.to > departedAt")
+        fun t8() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 1)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 17, 59)
+
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
+
+            val results = apply(arrivedAt, departedAt, timeBase)
+
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(LocalDateTime.of(2024, 10, 1, 9, 1))
+                assertThat(it.to)
+                    .isEqualTo(departedAt)
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
+        }
+
+        @Test
+        @DisplayName("arrivedAt < timeBase.from && timeBase.to < departedAt")
+        fun t9() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 9, 1)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 18, 1)
+
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0, 0))
+
+            val results = apply(arrivedAt, departedAt, timeBase)
+
+            results.first().let {
+                assertThat(it.from)
+                    .isEqualTo(arrivedAt)
+                assertThat(it.to)
+                    .isEqualTo(departedAt.toLocalDate().atTime(timeBase.to))
+            }
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
+        }
+
+        @Test
+        @DisplayName("arrivedAt, departedAt not in all timeBase && crossesMidnight = false")
+        fun t10() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 0, 0)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 1, 0)
+
+            val timeBase = TimeBase(LocalTime.of(9, 0), LocalTime.of(18, 0))
+
+            val results = apply(arrivedAt, departedAt, timeBase)
+
+            assertThat(results).isEmpty()
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
+        }
+
+        @Test
+        @DisplayName("arrivedAt, departedAt not in all timeBase && crossesMidnight = true")
+        fun t11() {
+            val arrivedAt = LocalDateTime.of(2024, 10, 1, 22, 0)
+            val departedAt = LocalDateTime.of(2024, 10, 1, 22, 59)
+
+            val timeBase = TimeBase(LocalTime.of(23, 0), LocalTime.of(1, 0))
+
+            val results = apply(arrivedAt, departedAt, timeBase)
+
+            assertThat(results).isEmpty()
+
+            results.forEach {
+                println("$it, 적용 시간: ${it.toMinutes}분")
+            }
+
+            val totalSumOfDurations = results.sumOf { it.toMinutes }
+            println("총 적용 시간: ${totalSumOfDurations}분")
+        }
     }
 
+    @Nested
+    inner class MultiDayTests
 
     fun apply(
         arrivedAt: LocalDateTime,
         departedAt: LocalDateTime,
-        timebase: Timebase,
+        timeBase: TimeBase,
     ): List<Result> {
         val beginDate = arrivedAt.toLocalDate()
         val endDate = departedAt.toLocalDate()
 
         val applies = mutableListOf<Result>()
+
         if (beginDate == endDate) {
-            analyze(arrivedAt, departedAt, timebase)?.let { applies.add(it) }
+            evaluation(arrivedAt, departedAt, timeBase)?.let { applies.add(it) }
         } else {
-            analyze(arrivedAt, beginDate.atTime(LocalTime.MAX), timebase)?.let { applies.add(it) }
+            evaluation(arrivedAt, beginDate.atTime(LocalTime.MAX), timeBase)?.let { applies.add(it) }
 
             var current = beginDate.plusDays(1)
             while (current.isBefore(endDate)) {
-                analyze(
+                evaluation(
                     current.atTime(LocalTime.MIN),
                     current.atTime(LocalTime.MAX),
-                    timebase
+                    timeBase,
                 )?.let { applies.add(it) }
                 current = current.plusDays(1)
             }
 
-            analyze(endDate.atStartOfDay(), departedAt, timebase)?.let { applies.add(it) }
+            evaluation(endDate.atStartOfDay(), departedAt, timeBase)?.let { applies.add(it) }
         }
 
         return applies.toList()
     }
 
-    data class Result(
-        val from: LocalDateTime,
-        val to: LocalDateTime,
-        val reason: Timebase,
-    ) {
-        val toMinutes get() = Duration.between(from, to).toMinutes()
-    }
-
-    fun analyze(
+    private fun evaluation(
         from: LocalDateTime,
         to: LocalDateTime,
-        timebase: Timebase,
+        timeBase: TimeBase,
     ): Result? {
-        println("$from ~ $to, $timebase")
-        if (from == to) {
+        println("$from ~ $to, $timeBase")
+
+        if (!timeBase.contains(from.toLocalTime()) && !timeBase.contains(to.toLocalTime())) {
             return null
         }
 
         var start: LocalDateTime = from
         var end: LocalDateTime = to
 
-        if (timebase.crossesMidnight) {
-            if (from.toLocalTime() <= timebase.from) {
-                start = from.toLocalDate().atTime(timebase.from)
+        if (timeBase.crossesMidnight) {
+            if (from.toLocalTime() <= timeBase.from) {
+                start = from.toLocalDate().atTime(timeBase.from)
                 if (start > to) {
                     start = from.toLocalDate().atStartOfDay()
                 }
@@ -183,71 +339,75 @@ class Tests {
             if (to.toLocalTime() == LocalTime.MAX) {
                 end = to.toLocalDate().plusDays(1).atStartOfDay()
             } else {
-                if (to.toLocalTime() > timebase.to) {
-                    end = to.toLocalDate().atTime(timebase.to)
+                if (to.toLocalTime() > timeBase.to) {
+                    end = to.toLocalDate().atTime(timeBase.to)
                 }
             }
 
-            return Result(start, end, timebase)
+            return Result(start, end, timeBase)
         } else {
-            if (from.toLocalTime() < timebase.from) {
-                start = from.toLocalDate().atTime(timebase.from)
+            if (from.toLocalTime() < timeBase.from) {
+                start = from.toLocalDate().atTime(timeBase.from)
             }
-
 
             if (to.toLocalTime() == LocalTime.MAX) {
-                if (timebase.to == LocalTime.MAX) {
+                if (timeBase.to == LocalTime.MAX) {
                     end = to.toLocalDate().plusDays(1).atStartOfDay()
                 }
-                if (to.toLocalTime() > timebase.to) {
-                    end = to.toLocalDate().atTime(timebase.to)
+                if (to.toLocalTime() > timeBase.to) {
+                    end = to.toLocalDate().atTime(timeBase.to)
                 }
             } else {
-                if (to.toLocalTime() > timebase.to) {
-                    end = to.toLocalDate().atTime(timebase.to)
+                if (to.toLocalTime() > timeBase.to) {
+                    end = to.toLocalDate().atTime(timeBase.to)
                 }
             }
 
-            return Result(start, end, timebase)
+            return Result(start, end, timeBase)
         }
     }
 
+    data class Result(
+        val from: LocalDateTime,
+        val to: LocalDateTime,
+        val reason: TimeBase,
+    ) {
+        val toMinutes get() = Duration.between(from, to).toMinutes()
+    }
+
     data class Plan(
-        val timebase: Timebase,
+        val timeBase: TimeBase,
         val policyType: PolicyType,
         val rate: Double,
     )
 
     enum class PolicyType {
         SURCHARGE,
-        DISCOUNT
+        DISCOUNT,
     }
-
 
     class DayOfWeekRule {
         val dayOfWeek: DayOfWeek
-        val slots: List<Timebase>
+        val slots: List<TimeBase>
         private val selectedDate: LocalDate?
 
-        constructor(selectedDate: LocalDate, timebases: List<Timebase>) {
+        constructor(selectedDate: LocalDate, timeBases: List<TimeBase>) {
             this.dayOfWeek = selectedDate.dayOfWeek
-            this.slots = timebases
+            this.slots = timeBases
             this.selectedDate = selectedDate
         }
 
-        constructor(dayOfWeek: DayOfWeek, timebases: List<Timebase>) {
+        constructor(dayOfWeek: DayOfWeek, timeBases: List<TimeBase>) {
             this.dayOfWeek = dayOfWeek
-            this.slots = timebases
+            this.slots = timeBases
             this.selectedDate = null
         }
 
-        fun isApplicable(date: LocalDate): Boolean {
-            return if (selectedDate != null) {
+        fun isApplicable(date: LocalDate): Boolean =
+            if (selectedDate != null) {
                 date == selectedDate
             } else {
                 date.dayOfWeek == dayOfWeek
             }
-        }
     }
-
 }
